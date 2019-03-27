57fa4877589436fd9a411b179c55b667b810a7d8
tom <caolvchong@gmail.com>
2019-03-27 13:16:57 +0800

    merge master-pc

--- config/webpack/postcss.config.js
+++ config/webpack/postcss.config.js
@@ -2,6 +2,7 @@ const custom = require('../webpack')
 module.exports = {
   ident: 'postcss',
   plugins: [
+    require('postcss-calc')(),
     require('postcss-import')(),
     require('precss')(),
     require('postcss-cssnext')(),
